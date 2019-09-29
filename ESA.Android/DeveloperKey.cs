736299e200ecd1c5beb078c6d4cbc8e47dfa1497
douglasTafeSA <douglas.hudson-walker@student.tafesa.edu.au>
2019-09-29 23:43:25 +0930

    Library Reference, R String Links

---
+++ ESA.Android/DeveloperKey.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESA
+{
+    public static class DeveloperKey
+    {
+        public static string Key = "AIzaSyCbn9np-1DuPwr4DGlY13JPcpLGPsge8pU";
+    }
+}
---
+++ ESA.Android/Screens/PlayerViewActivity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Xamarin.Forms;
+
+using Com.Google.Android.Youtube.Player;
+
+namespace ESA.Droid.Screens
+{
+    [Activity(Label = "PlayerViewActivity")]
+    public class PlayerViewActivity : YouTubeFailureRecoveryActivity
+    {
+        #region implemented abstract members of YouTubeFailureRecoveryActivity
+
+        protected override IYouTubePlayerProvider GetYouTubePlayerProvider()
+        {
+            return FindViewById<YouTubePlayerView>(Resource.Id.youtube_view);
+        }
+
+        #endregion
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Create your application here
+            SetContentView(Resource.Layout.PlayerView);
+
+            YouTubePlayerView youTubeView = FindViewById<YouTubePlayerView>(Resource.Id.youtube_view);
+            youTubeView.Initialize(DeveloperKey.Key, this);
+        }
+
+        public override void OnInitializationSuccess(IYouTubePlayerProvider provider, IYouTubePlayer player, bool wasRestored)
+        {
+            if (!wasRestored)
+            {
+                player.CueVideo("wKJ9KzGQq0w");
+            }
+        }
+    }
+}
