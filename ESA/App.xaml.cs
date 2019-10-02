79736c23edf8803dcfbf013e145abd54ac7fd905
Cassi <cassandra.papageorgiou@student.tafesa.edu.au>
2019-10-03 00:12:53 +0930

    Combined Main Page Prototypes based on Team Meeting Feedback

--- ESA/App.xaml.cs
+++ ESA/App.xaml.cs
@@ -10,7 +10,8 @@ namespace ESA
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- ESA/MainPage.xaml.cs
+++ ESA/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ESA.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -16,6 +17,35 @@ namespace ESA
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void AboutUs_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AboutPage());
+        }
+
+        private async void Video_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new VideoPage());
+        }
+
+        private async void Lacrimal_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CategoriesPage());
+        }
+        private async void Orbital_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CategoriesPage());
+        }
+
+        private async void Eyelid_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CategoriesPage());
+        }
+
+        private async void Search_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CategoriesPage());
         }
     }
 }
