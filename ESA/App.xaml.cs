77808861f3573c8d08032ed21cd572830d18b8fa
Robbie Benson <shoukin11@gmail.com>
2019-10-03 09:26:55 +0930

    Added Layout for Home Page and Finished About Page (including data binding)

--- ESA/App.xaml.cs
+++ ESA/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ESA.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace ESA
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
