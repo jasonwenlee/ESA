cd8e99ef4fbd5c21b459ce057ba754450db0e973
ALEXANDRA FILIPPOU <Alexandra.FILIPPOU@student.tafesa.edu.au>
2019-10-07 21:53:30 +1030

    The 2 branches updated, new photos provided bu client included and Hyperlink form Keypoint linked as an example to https://oculoplastic.eyesurgeryvideos.net

--- ESA/Models/Complications.cs
+++ ESA/Models/Complications.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
 using System.Text;
+using ESA.Views;
+using System.Reflection;
 
 namespace ESA.Models
 {
@@ -10,10 +14,40 @@ namespace ESA.Models
         public string ImageUrl { get; set; }
 
 
-
-        public override string ToString()
+        public  List<Complications> GetComplicationsList()
         {
-            return DescPhotoText;
+            List<Complications> complicationView = new List<Complications>()
+            {
+                new Complications()
+                {
+                    DescPhotoText = "• Upper eyelid retraction",
+                    ImageUrl = "eyeRetraction01.jpeg"
+                },
+                new Complications()
+                {
+                     DescPhotoText = "• Conjunctiva advancing anteriorly over lid margin",
+                    ImageUrl = "Complication02.jpeg"
+                },
+
+                new Complications()
+                {
+                    DescPhotoText = "• Skin advancing posteriorly over lid margin",
+                    ImageUrl = "Complication03.jpeg"
+                },
+
+                
+                  new Complications()
+                {
+                     DescPhotoText = "• Lid margin granuloma",
+                    ImageUrl = "eyeLidGranuloma04.jpg"
+                },
+                    new Complications()
+                {
+                     DescPhotoText = "• Lid margin irregularity",
+                    ImageUrl = "eyeLidMarginIrregularity05.jpg"
+                }
+            };
+            return complicationView;
         }
     }
 }
