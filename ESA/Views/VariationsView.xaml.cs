c726c5a8f8e5cf4e987107bb0e565a1b2018bace
Hyun S JANG <hyunsjang@hotmail.com>
2019-10-09 17:07:41 +1030

    Added sentences in variation page writing space, and commented out unnecessary codes in Models/VariationPageModel.cs and ViewModels/VariationPageViewModel.cs. However, image does not show in variation page, so need to be fixed.

--- ESA/Views/VariationsView.xaml.cs
+++ ESA/Views/VariationsView.xaml.cs
@@ -1,4 +1,4 @@
-﻿using ESA.ViewModels;
+﻿using ESA.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +13,63 @@ namespace ESA.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class VariationsView : ContentView
     {
-    //    VariationPageViewModel vm;
+        public IList<VariationPageModel> VariationPageModels { get; set; }
+        //    VariationPageViewModel vm;
         public VariationsView()
         {
             InitializeComponent();
-               BindingContext = new VariationPageViewModel();
-  //          vm = new VariationPageViewModel();
+            VariationPageModels = new List<VariationPageModel>();
 
-     //       VariationList.ItemsSource = vm.VariationsView;
-     
-    //        BindingContext = vm;
+            VariationPageModels.Add(new Models.VariationPageModel
+
+            {
+                Title = "• Upper eyelid retraction",
+                Details = "ESA.Resources.Complication01.jpeg"
+
+
+            });
+            VariationPageModels.Add(new Models.VariationPageModel
+
+            {
+                Title = "• Conjunctiva advancing anteriorly over lid margin",
+                Details = "ESA.Resources.Complication02.jpeg"
+
+
+            });
+            VariationPageModels.Add(new Models.VariationPageModel
+
+            {
+                Title = "• Skin advancing posteriorly over lid margin​",
+                Details = "ESA.Resources.Complication03.jpeg"
+
+
+            });
+
+            VariationPageModels.Add(new Models.VariationPageModel
+
+            {
+                Title = "• Lid margin granuloma ​",
+                Details = "ESA.Resources.Complication04.jpeg"
+
+
+            });
+
+            
+            BindingContext = this;
+
         }
+        //BindingContext = new VariationPageViewModel();
+        //          vm = new VariationPageViewModel();
 
-        ListView HeaderList = new ListView()
-        {
-            Header = "Related Procedure",
-            Footer = "Footer"
-        };
+        //       VariationList.ItemsSource = vm.VariationsView;
+
+        //        BindingContext = vm;
     }
+
+        //ListView HeaderList = new ListView()
+        //{
+        //    Header = "Related Procedure",
+        //    Footer = "Footer"
+        //};
+    
 }
