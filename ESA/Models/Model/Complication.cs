c58845b4682b9b1361f14934bf1a1b3585d8953c
douglasTafeSA <douglas.hudson-walker@student.tafesa.edu.au>
2019-11-03 14:11:28 +1030

    Complications model fix

--- ESA/Models/Model/Complication.cs
+++ ESA/Models/Model/Complication.cs
@@ -14,77 +14,91 @@ namespace ESA.Models.Model
 
         internal List<Complication> GetComplications(int id)
         {
-            List<Complication> complicationsList = new List<Complication>()
+            List<Complication> complications = new List<Complication>();
+            switch (id)
             {
-                // Tarso
-              
-                    new Complication()
-                    {   Number=1,
-                        Name = "Upper eyelid retraction",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    },
-                    new Complication()
-                    { Number=2,
-                        Name = "Skin advancing posteriorly over lid margin",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.SkinAdvancingOver.jpeg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    },
-                    new Complication()
-                    {    Number=3,
-                        Name = "Lid margin granuloma",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    },
-                    new Complication()
-                    { Number=4,
-                        Name = "Lid margin irregularity",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginIrregularity.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    },
-                    new Complication()
-                    { Number=5,
-                        Name = "Conjunctiva advancing anteriorly over lid margin",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    }
-                };
-            // Tenzel
-            new List<Complication>()
-                {
-                    new Complication()
-                    {   Number=1,
-                        Name = "Ectropion",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    },
-                    new Complication()
-                    { Number=2,
-                        Name = "Pyogenic Granuloma",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.SkinAdvancingOver.jpeg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    },
-                    new Complication()
-                    {  Number=3,
-                        Name = "Symblepharon",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    },
-                    new Complication()
-                    { Number=4,
-                        Name = "Marginal Notching",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginIrregularity.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    }
-                };
-                // Graft
-                new List<Complication>()
-                {
-                    new Complication()
-                    { Number=1,
-                        Name = "Wound Dehiscence",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    },
-                    new Complication()
-                    { Number=2,
-                        Name = "Wound Haematoma",
-                        Image = ImageSource.FromResource("ESA.Resources.Complications.SkinAdvancingOver.jpeg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
-                    }
-                
-            };
+                case 0:
+                    complications = new List<Complication>()
+                    {
+                        new Complication()
+                        {
+                            Number = 1,
+                            Name = "Upper eyelid retraction",
+                            Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                        },
+                        new Complication()
+                        {
+                            Number = 2,
+                            Name = "Skin advancing posteriorly over lid margin",
+                            Image = ImageSource.FromResource("ESA.Resources.Complications.SkinAdvancingOver.jpeg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                        },
+                        new Complication()
+                        {
+                            Number = 3,
+                            Name = "Lid margin granuloma",
+                            Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                        },
+                        new Complication()
+                        {
+                            Number = 4,
+                            Name = "Lid margin irregularity",
+                            Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginIrregularity.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                        },
+                        new Complication()
+                        {
+                            Number = 5,
+                            Name = "Conjunctiva advancing anteriorly over lid margin",
+                            Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                        }
+                    };
+                    break;
+                case 1:
+                    // Tenzel
+                    complications = new List<Complication>()
+                        {
+                            new Complication()
+                            {   Number=1,
+                                Name = "Ectropion",
+                                Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                            },
+                            new Complication()
+                            { Number=2,
+                                Name = "Pyogenic Granuloma",
+                                Image = ImageSource.FromResource("ESA.Resources.Complications.SkinAdvancingOver.jpeg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                            },
+                            new Complication()
+                            {  Number=3,
+                                Name = "Symblepharon",
+                                Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                            },
+                            new Complication()
+                            { Number=4,
+                                Name = "Marginal Notching",
+                                Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginIrregularity.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                            }
+                        };
+                    break;
 
-            return complicationsList; 
+                case 2:
+                    // Graft
+                    complications = new List<Complication>()
+                    {
+                        new Complication()
+                        { Number=1,
+                            Name = "Wound Dehiscence",
+                            Image = ImageSource.FromResource("ESA.Resources.Complications.LidMarginGranuloma.jpg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                        },
+                        new Complication()
+                        { Number=2,
+                            Name = "Wound Haematoma",
+                            Image = ImageSource.FromResource("ESA.Resources.Complications.SkinAdvancingOver.jpeg", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+                        }
+                     };
+                    break;
+            }
+
+
+            return complications;
         }
     }
 }
