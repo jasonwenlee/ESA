214bfa5f1d007e6831e8a149f2b449b598b1dd52
Roeche <Roeche@yahoo.com>
2019-11-23 12:30:43 +1030

    Final version of Splash screen. Black background and no Bouncing as per Valerie's request.

--- ESA/SplashPage.cs
+++ ESA/SplashPage.cs
@@ -24,7 +24,7 @@ namespace ESA
             splashImage = new Image
             {
                 //Source = "sqEye.png", // REC: Substitution of original "squared" eye.
-                Source = "circEye_v3.png", // REC: Circular eye
+                Source = "circeye_v3.png", // REC: Circular eye
                 WidthRequest = 100,
                 HeightRequest = 100
             };
@@ -43,10 +43,9 @@ namespace ESA
         {
             base.OnAppearing();
             
-            await splashImage.ScaleTo(1.0, 1000); // REC: Setting up the initial eye image (size,time). 
+            await splashImage.ScaleTo(1.0, 800); // REC: Setting up the initial eye image (size,time 1000 = 1 sec). 
             // await splashImage.ScaleTo(0.8, 200, Easing.Linear); // REC: Animation effect: reducing image. Eliminated to avoid "bouncing".
-            await splashImage.ScaleTo(2.5, 500, Easing.Linear); // REC: Animation effect: augmenting image. Final (size,time).
-
+            await splashImage.ScaleTo(2.5, 1000, Easing.CubicInOut); // REC: final state (size,time). CubicInOut = Natural looking choice. 
             Application.Current.MainPage = new NavigationPage(new MainPage());
         }
     }
