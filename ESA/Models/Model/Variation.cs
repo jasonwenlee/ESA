c60076b4c003d4fd05df0d16a20ae01ff8069932
douglasTafeSA <douglas.hudson-walker@student.tafesa.edu.au>
2019-12-02 13:27:44 +1030

    Variations, Related Procedures and Text italics

--- ESA/Models/Model/Variation.cs
+++ ESA/Models/Model/Variation.cs
@@ -8,62 +8,151 @@ namespace ESA.Models.Model
     public class Variation
     {
         public FormattedString Content { get; set; }
+        public List<RelatedProcedure> RelatedProcedures { get; set; } = null;
 
         internal List<Variation> GetVariations(int id)
         {
             List<Variation> varsd = new List<Variation>();
             //List<VariationsModel> vars = new List<VariationsModel>();
+            FormattedString fString = new FormattedString();
 
             switch (id)
             {
                 case 0:
                     // Tarso Hughes
-                    FormattedString fString = new FormattedString();
+                    
                     fString.Spans.Add(new Span() { Text = "Size of flap", Style = (Style)Application.Current.Resources["VariationHeader"] });
-                    fString.Spans.Add(new Span() { Text = "\nWider flap may need a straighter horizonal incision without vertical relieving incisions.", Style = (Style)Application.Current.Resources["VariationSubHeader"] });
-                    varsd.Add(new Variation() { Content = fString });
-
-                    fString = new FormattedString();
-                    fString.Spans.Add(new Span() { Text = "Tarsoconjunctival-based or Tarsoconjunctival-Muller-based flap​.", Style = (Style)Application.Current.Resources["VariationHeader"] });
-                    fString.Spans.Add(new Span() { Text = "\nConjunctiva and Mullers together allow better vascularsation of the flap.", Style = (Style)Application.Current.Resources["VariationSubHeader"] });
-                    fString.Spans.Add(new Span() { Text = "\nIf advancing Mullers, which is an eyelid retractor, there may be a higher risk of post operative eyelid retraction that advancing conjunctiva alone.", Style = (Style)Application.Current.Resources["VariationSubHeader"] });
-                    varsd.Add(new Variation() { Content = fString });
-  
+                    fString.Spans.Add(new Span() { Text = "\nWider flap may need a straighter horizonal incision without vertical relieving incisions.", Style = (Style)Application.Current.Resources["VariationContent"] });
+                    fString.Spans.Add(new Span() { Text = "\nTarsoconjunctival-based or Tarsoconjunctival-Muller-based flap​.", Style = (Style)Application.Current.Resources["VariationHeader"] });
+                    fString.Spans.Add(new Span() { Text = "\nConjunctiva and Mullers together allow better vascularsation of the flap.", Style = (Style)Application.Current.Resources["VariationContent"] });
+                    fString.Spans.Add(new Span() { Text = "\nIf advancing Mullers, which is an eyelid retractor, there may be a higher risk of post operative eyelid retraction that advancing conjunctiva alone.", Style = (Style)Application.Current.Resources["VariationContent"] });
+                    varsd.Add(new Variation() { 
+                        Content = fString,
+                        RelatedProcedures = new List<RelatedProcedure>()
+                        {
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Tarsoconjuntival (Hughes) flap - stage II",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Tarsonconjunctical (Hughes) Flap - Stage II",
+                                Id = 1
+                            },
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Hewes flap",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Hewes flap",
+                                Id = 2
+                            }
+                        }
+                    });
                     break;
                 case 1:
                     // Tenzel
                     
                     {
-                        FormattedString gString = new FormattedString();
-                        gString.Spans.Add(new Span() { Text = "Defects greater on the lower than upper eyelid"});
-                        varsd.Add(new Variation() { Content = gString });
-
-                        gString = new FormattedString();
-                        gString.Spans.Add(new Span() { Text = "Any lid defect - Lateral/Central/Medial", Style = (Style)Application.Current.Resources["VariationHeader"] });
-                        gString.Spans.Add(new Span() { Text = "\nCentral to Medial", Style = (Style)Application.Current.Resources["VariationSubHeader"] });
-                        varsd.Add(new Variation() { Content = gString });
-
-                        gString = new FormattedString();
-                        gString.Spans.Add(new Span() { Text = "Moving eyelid from lateral to Medial"});
-                        varsd.Add(new Variation() { Content = gString });
+                        fString.Spans.Add(new Span() { Text = "Defects greater on the lower than upper eyelid"});
+                        fString.Spans.Add(new Span() { Text = "\nAny lid defect - Lateral/Central/Medial", Style = (Style)Application.Current.Resources["VariationHeader"] });
+                        fString.Spans.Add(new Span() { Text = "\nCentral to Medial", Style = (Style)Application.Current.Resources["VariationContent"] });
+                        fString.Spans.Add(new Span() { Text = "\nMoving eyelid from lateral to Medial", Style = (Style)Application.Current.Resources["VariationContent"] });
+                        varsd.Add(new Variation()
+                        {
+                            Content = fString,
+                            RelatedProcedures = new List<RelatedProcedure>()
+                        {
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Periosteal flaps",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Periosteal flaps",
+                                Id = 2
+                            },
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Direct closure",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Direct closure",
+                                Id = 0
+                            }
+                        }
+                        }); ;
                     };
                     break;
                 case 2:
                     // Graft
                     
                     {
-                        FormattedString hString = new FormattedString();
-                        hString.Spans.Add(new Span() { Text = "Variations in steps", Style = (Style)Application.Current.Resources["VariationHeader"] });
-                        hString.Spans.Add(new Span() { Text = "\nDonor site – upper eyelid, post auricular, supraclavicular, inner arm" , Style = (Style)Application.Current.Resources["VariationSubHeader"] });
-                        hString.Spans.Add(new Span() { Text = "\nLarge grafts – small perforations can be made into the graft to reduce the risk of haematoma collection and graft failure" , Style = (Style)Application.Current.Resources["VariationSubHeader"] });
-                        varsd.Add(new Variation() { Content = hString });
+                        fString.Spans.Add(new Span() { Text = "Variations in steps", Style = (Style)Application.Current.Resources["VariationHeader"] });
+                        fString.Spans.Add(new Span() { Text = "\nDonor site – upper eyelid, post auricular, supraclavicular, inner arm" , Style = (Style)Application.Current.Resources["VariationContent"] });
+                        fString.Spans.Add(new Span() { Text = "\nLarge grafts – small perforations can be made into the graft to reduce the risk of haematoma collection and graft failure" , Style = (Style)Application.Current.Resources["VariationContent"] });
+                        varsd.Add(new Variation()
+                        {
+                            Content = fString,
+                            RelatedProcedures = new List<RelatedProcedure>()
+                        {
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Split thickness skin graft",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Split thickness skin graft",
+                                Id = 1
+                            },
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Free tarsal graft",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Free tarsal graft",
+                                Id = 0
+                            },
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Full thickness mucosal graft",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Full thickness mucosal graft",
+                                Id = 1
+                            },
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Split thickness mucosal graft",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Split thickness mucosal graft",
+                                Id = 0
+                            },
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Hard palate mucosal graft",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Hard palate mucosal graft",
+                                Id = 1
+                            },
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Hewes flap",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Hewes flap",
+                                Id = 0
+                            },
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Nasal mucosal graft",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Nasal mucosal graft",
+                                Id = 1
+                            },
+                            new RelatedProcedure()
+                            {
+                                ProcedureLink = "Ear cartilage graft",
+                                Details = new Detail() { UploadDate = DateTime.Parse("2015/09/24"), ViewCount = 467 },
+                                ProcedureName = "Ear cartilage graft",
+                                Id = 0
+                            },
+                        }
+                        });
                     };
                     break;
 
-
-
-
             }
+
 
             //    // Tarso
             //    new List<Variation>()
--- ESA/Views/VariationsView.xaml.cs
+++ ESA/Views/VariationsView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using ESA.Models;
+using ESA.Models.CustomRenderers;
+using ESA.Models.Model;
 using ESA.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -14,15 +16,24 @@ namespace ESA.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class VariationsView : ContentView
     {
+        ProcedureViewModel procedureViewModel;
         public VariationsView(ProcedureViewModel viewModel)
         {
             InitializeComponent();
-            BindingContext = viewModel;
+            procedureViewModel = viewModel;
+            BindingContext = procedureViewModel;
         }
 
         private void RelatedProcedureButton_Tapped(object sender, EventArgs e)
         {
             Navigation.PushAsync(new DetailsPage(1));
+        }
+
+        private void RelatedProcedureButton_Clicked(object sender, EventArgs e)
+        {
+            Variation variation = procedureViewModel.Procedure.Variations[0];
+            int procedureId = variation.RelatedProcedures.First(rp => rp.ProcedureLink == ((CustomButton)sender).Text).Id;
+            Navigation.PushAsync(new DetailsPage(procedureId));
         }
     }
 }
