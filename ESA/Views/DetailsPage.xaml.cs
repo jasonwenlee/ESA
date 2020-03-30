a6e2d5c6a2eb0e147c71a42aaaa2c45e344ec369
douglasTafeSA <douglas.hudson-walker@student.tafesa.edu.au>
2020-03-30 18:36:44 +1030

    Video Controls Enlarged Fade timer duration incresed to 3000

--- ESA/Views/DetailsPage.xaml.cs
+++ ESA/Views/DetailsPage.xaml.cs
@@ -60,7 +60,7 @@ namespace ESA.Views
             BindingContext = procedureViewModel;
 
             // Fade Timer
-            fadeTimer.Interval = 2000;
+            fadeTimer.Interval = 3000;
             fadeTimer.Elapsed += (s, e) =>
             {
                 Device.BeginInvokeOnMainThread(() => { VideoControls_Tapped(s, e); });
